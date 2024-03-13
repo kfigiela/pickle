a66855504d5b7160f0ac1929d11dc976e10a388b
Kamil Figiela <kamil.figiela@soostone.com>
2024-03-13 12:19:53 +0000

    Update dev environment

--- shell.nix
+++
@@ -1,9 +0,0 @@
-let pkgsrc = builtins.fetchGit
-    {
-        url = "https://github.com/nixos/nixpkgs";
-        ref = "master";
-        rev = "13b815603077abd0bcfcf412f9fbb28df2320ff3";
-    };
-    pkgs = import pkgsrc {};
-in with pkgs;
-(haskell.packages.ghc865.callPackage (import ./default.nix) {}).env
